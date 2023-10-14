0949fa8b0e2c8a20862f8f8eae678a64712f211f
Hiram <116443893+Mvg4m61@users.noreply.github.com>
2023-10-14 22:02:07 +0300

    Update jacobi_iteration.py

--- jacobi_iteration.py
+++ jacobi_iteration.py
@@ -81,4 +81,35 @@ def jacobi_iteration_method(
 
     rows1, cols1 = coefficient_matrix.shape
     rows2, cols2 = constant_matrix.shape
+  if rows1 != cols1:
+        msg = f"Coefficient matrix dimensions must be nxn but received {rows1}x{cols1}"
+        raise ValueError(msg)
 
+    if cols2 != 1:
+        msg = f"Constant matrix must be nx1 but received {rows2}x{cols2}"
+        raise ValueError(msg)
+
+    if rows1 != rows2:
+        msg = (
+            "Coefficient and constant matrices dimensions must be nxn and nx1 but "
+            f"received {rows1}x{cols1} and {rows2}x{cols2}"
+        )
+        raise ValueError(msg)
+
+    if len(init_val) != rows1:
+        msg = (
+            "Number of initial values must be equal to number of rows in coefficient "
+            f"matrix but received {len(init_val)} and {rows1}"
+        )
+        raise ValueError(msg)
+
+    if iterations <= 0:
+        raise ValueError("Iterations must be at least 1")
+
+    table: NDArray[float64] = np.concatenate(
+        (coefficient_matrix, constant_matrix), axis=1
+    )
+
+    rows, cols = table.shape
+
+    strictly_diagonally_dominant(table)
