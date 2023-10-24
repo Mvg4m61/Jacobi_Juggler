80def6319b6c2ee71a40d93c55595c76cfd8ac08
Hiram <116443893+Mvg4m61@users.noreply.github.com>
2023-10-24 23:14:18 +0300

    Update jacobi_iteration.py

--- jacobi_iteration.py
+++ jacobi_iteration.py
@@ -3,7 +3,9 @@ Jacobi Iteration Method - https://en.wikipedia.org/wiki/Jacobi_method
 """
 from __future__ import annotations
 
-
+import numpy as np
+from numpy import float64
+from numpy.typing import NDArray
 
 # Method to find solution of system of linear equations
 def jacobi_iteration_method(
